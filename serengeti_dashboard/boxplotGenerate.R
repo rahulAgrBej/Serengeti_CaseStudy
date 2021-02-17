61b2114312e5c5ea62ef036d1145451d822019e7
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-02-16 22:53:08 -0500

    fully integrated boxplots into dashboard

--- serengeti_dashboard/boxplotGenerate.R
+++ serengeti_dashboard/boxplotGenerate.R
@@ -1,50 +1,57 @@
 
-# Code from the boxplot app.R written by Sadie Herbert
+# Rearranging and adapting code snippet from the boxplot app.R written by Sadie Herbert
+
+getSerengetiData <- function(fp) {
+  
+  data <- read_csv(fp) 
+  
+  
+  names(data) <- gsub(" ", "_", names(data))
+  names(data) <- gsub("\\(", "", names(data))
+  names(data) <- gsub("\\)", "", names(data))
+  
+  data <- data %>% 
+    mutate(Date = mdy(Date),
+           Species = factor(Species),
+           Standing = factor(Standing,
+                             levels = c(0, 1),
+                             labels = c("Not standing", "Standing")),
+           Resting = factor(Resting,
+                            levels = c(0, 1),
+                            labels = c("Not resting", "Resting")),
+           Moving = factor(Moving,
+                           levels = c(0, 1),
+                           labels = c("Not moving", "Moving")),
+           Eating = factor(Eating,
+                           levels = c(0, 1),
+                           labels = c("Not eating", "Eating")),
+           Interacting = factor(Interacting,
+                                levels = c(0, 1),
+                                labels = c("Not interacting", "Interacting")),
+           Babies = factor(Babies,
+                           levels = c(0, 1),
+                           labels = c("No babies", "Babies")),
+           Habitat = factor(Habitat),
+           Log_Number_Of_Animals = log(Number_Of_Animals),
+           Log_Longitude_m = log(Longitude_m),
+           Log_Latitude_m = log(Latitude_m),
+           Log_Amount_of_Shade = log(Amount_of_Shade),
+           Log_Distance_to_River_m = log(Distance_to_River_m),
+           Log_Distance_to_Confluence_m = log(Distance_to_Confluence_m),
+           Log_Distance_to_Kopje_m = log(Distance_to_Kopje_m),
+           Log_Tree_Density_Measure = log(Tree_Density_Measure),
+           Log_Lion_Risk_Wet = log(Lion_Risk_Wet),
+           Log_Lion_Risk_Dry = log(Lion_Risk_Dry),
+           Log_Greeness_Wet = log(Greeness_Wet),
+           Log_Greeness_Dry = log(Greeness_Dry))
+  
+  return(data)
+}
+
+ss_data <- getSerengetiData('../../Full_Serengeti_Data.csv')
 
 # A colorblind-friendly palette with grey:
 cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999")
-
-ss_data <- read_csv("../../Full_Serengeti_Data.csv")
-
-names(ss_data) <- gsub(" ", "_", names(ss_data))
-names(ss_data) <- gsub("\\(", "", names(ss_data))
-names(ss_data) <- gsub("\\)", "", names(ss_data))
-
-ss_data <- ss_data %>% 
-  mutate(Date = mdy(Date),
-         Species = factor(Species),
-         Standing = factor(Standing,
-                           levels = c(0, 1),
-                           labels = c("Not standing", "Standing")),
-         Resting = factor(Resting,
-                          levels = c(0, 1),
-                          labels = c("Not resting", "Resting")),
-         Moving = factor(Moving,
-                         levels = c(0, 1),
-                         labels = c("Not moving", "Moving")),
-         Eating = factor(Eating,
-                         levels = c(0, 1),
-                         labels = c("Not eating", "Eating")),
-         Interacting = factor(Interacting,
-                              levels = c(0, 1),
-                              labels = c("Not interacting", "Interacting")),
-         Babies = factor(Babies,
-                         levels = c(0, 1),
-                         labels = c("No babies", "Babies")),
-         Habitat = factor(Habitat),
-         Log_Number_Of_Animals = log(Number_Of_Animals),
-         Log_Longitude_m = log(Longitude_m),
-         Log_Latitude_m = log(Latitude_m),
-         Log_Amount_of_Shade = log(Amount_of_Shade),
-         Log_Distance_to_River_m = log(Distance_to_River_m),
-         Log_Distance_to_Confluence_m = log(Distance_to_Confluence_m),
-         Log_Distance_to_Kopje_m = log(Distance_to_Kopje_m),
-         Log_Tree_Density_Measure = log(Tree_Density_Measure),
-         Log_Lion_Risk_Wet = log(Lion_Risk_Wet),
-         Log_Lion_Risk_Dry = log(Lion_Risk_Dry),
-         Log_Greeness_Wet = log(Greeness_Wet),
-         Log_Greeness_Dry = log(Greeness_Dry))
-
 
 continuous_vars <- c("Amount_of_Shade",
                      "Distance_to_Confluence_m",
@@ -79,6 +86,52 @@ categorical_vars <- c("Babies",
                       "Moving",
                       "Resting",
                       "Standing")
-#============================================================================
 
 species_list <- levels(ss_data$Species)
+
+boxplotCreate <- function(
+  species_input,
+  standing_input,
+  resting_input,
+  moving_input,
+  eating_input,
+  interacting_input,
+  babies_input,
+  habitat_input,
+  date_input,
+  x_input,
+  y_input) {
+  
+  ss_data_box <- getSerengetiData('../../Full_Serengeti_Data.csv')
+  ss_data_filtered <- ss_data_box %>% 
+    filter(Species %in% species_input) %>%
+    filter(Standing %in% standing_input) %>% 
+    filter(Resting %in% resting_input) %>%
+    filter(Moving %in% moving_input) %>%
+    filter(Eating %in% eating_input) %>%
+    filter(Interacting %in% interacting_input) %>%
+    filter(Babies %in% babies_input) %>%
+    filter(Habitat %in% habitat_input) %>%
+    filter(Date >= date_input[1] & Date <= date_input[2])
+  
+  p <- ss_data_filtered %>%
+    ggplot(aes_string(x = x_input, y = y_input)) +
+    geom_boxplot(aes(color = Species)) +
+    scale_color_manual(values = cbPalette) +
+    theme_minimal() +
+    scale_x_discrete(labels = label_wrap(10)) +
+    theme(axis.title = element_text(face = "bold", size = 24),
+          axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)),
+          axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
+          axis.text.x = element_text(size = 20),
+          axis.text.y = element_text(size = 20),
+          legend.text = element_text(size = 20),
+          legend.title = element_text(face = "bold", size = 24),
+          plot.margin = unit(c(1,1,1,1), "cm"))
+  
+  return(p)
+}
+
+#============================================================================
+
+
--- serengeti_dashboard/server.R
+++ serengeti_dashboard/server.R
@@ -1,6 +1,21 @@
 
 source('violinGenerate.R')
+source('boxplotGenerate.R')
 
 server <- function(input, output) {
   output$violinPredPrey <- renderPlot({ dailyViolinPlot(input$species_select) })
+  output$boxplotRender <- renderPlot({ boxplotCreate(
+    input$species_box,
+    input$standing_box,
+    input$resting_box,
+    input$moving_box,
+    input$eating_box,
+    input$interacting_box,
+    input$babies_box,
+    input$habitat_box,
+    input$date_box,
+    input$x_box,
+    input$y_box
+  ) })
+  
 }
