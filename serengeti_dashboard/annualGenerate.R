231d563d7fedd13cbecf9513804475fae973347b
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-03-10 21:28:47 -0500

    x axis months showing nums - remember this is a continuous scale

--- serengeti_dashboard/annualGenerate.R
+++ serengeti_dashboard/annualGenerate.R
@@ -50,7 +50,7 @@ annualplotCreate <- function(
     geom_col(aes(fill = Species), position = position_dodge()) +
     scale_fill_manual(values = cbPalette) +
     theme_minimal() +
-    scale_x_discrete(labels = label_wrap(10)) +
+    scale_x_continuous(breaks=seq(1, 12)) +
     theme(axis.title = element_text(face = "bold", size = 24),
           axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)),
           axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
